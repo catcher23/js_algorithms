4e033472d3904d731a517a7434ff757343461a8e
Danny <danlau168@gmail.com>
2016-01-19 15:20:04 -0800

    queue 2 stacks

---
+++ queue_two_stacks.js
@@ -0,0 +1,47 @@
+function Stack() {
+  var items = [];
+  this.push = function(el) {
+    items.push(el);
+  };
+  this.pop = function(){
+      return items.pop();
+  };
+
+  this.peek = function(){
+      return items[items.length-1];
+  };
+
+  this.isEmpty = function(){
+      return items.length === 0;
+  };
+
+  this.size = function(){
+      return items.length;
+  };
+
+  this.clear = function(){
+      items = [];
+    };
+}
+
+function Queue(inStack, outStack) {
+  this.enqueue = function(el) {
+    return inStack.push(el);
+  };
+  this.dequeue = function() {
+    if (outStack.isEmpty) {
+      while (inStack.size > 0) {
+        outStack.push(inStack.pop);
+      }
+    }
+    return outStack.pop;
+  };
+}
+
+var inStack = new Stack();
+var outStack = new Stack();
+
+// Implement a queue ↴ with 2 stacks ↴ . Your queue should have an enqueue and a dequeue function and it should be "first in first out" (FIFO).
+// Optimize for the time cost of mm function calls on your queue. These can be any mix of enqueue and dequeue calls.
+//
+// Assume you already have a stack implementation and it gives O(1)O(1) time push and pop.
