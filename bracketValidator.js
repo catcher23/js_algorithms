b23b0200ff1d1fd0242f8a9d888fe51910e7abd0
Danny <danlau168@gmail.com>
2016-01-25 18:30:07 -0800

    bracket validator

--- bracketValidator.js
+++ bracketValidator.js
@@ -1,17 +1,37 @@
 function isValid (input) {
 
-  var brackets = Set.new(),
-  set = Set.new(),
+  var
+  bracketHash = {
+    '{' : '}',
+    '[' : ']',
+    '(' : ')',
+  },
+  preBrackets = new Set(),
+  postBrackets = new Set(),
+  stack = [],
+  char,
+  last,
   i;
-
-  brackets.add('{')
-  brackets.add('[')
-  brackets.add('(')
+  preBrackets.add('{');
+  preBrackets.add('[');
+  preBrackets.add('(');
+  postBrackets.add('}');
+  postBrackets.add(']');
+  postBrackets.add(')');
 
   for (i = 0; i < input.length; i++) {
-    if (input[i] )
+    char = input[i];
+    if (preBrackets.has(char)) {
+      stack.push(char);
+    } else if (postBrackets.has(char)) {
+      last = stack[stack.length - 1];
+      if (bracketHash[last] !== char) {
+        return false;
+      }
+      stack.pop();
+    }
   }
-
+  return stack.length === 0 ? true : false;
 }
 
-console.log(isValid('[{()}]');
+console.log(isValid('[{()[}]'));
