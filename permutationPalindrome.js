4a39cac22106ea6aac33edc45e788927afc568ca
Danny <danlau168@gmail.com>
2016-02-01 21:42:01 -0800

    permutation palindrome

---
+++ permutationPalindrome.js
@@ -0,0 +1,8 @@
+// Write an efficient function that checks whether any permutation of an input
+//string is a palindrome .
+// Examples:
+//
+// "civic" should return true
+// "ivicc" should return true
+// "civil" should return false
+// "livci" should return false
